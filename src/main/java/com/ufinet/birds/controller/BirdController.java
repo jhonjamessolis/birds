c94d8a09ca4632ac34cc2b277aa701732b45f694
Jhon James Solis <jhonjamessolis@gmail.com>
2025-03-05 01:47:56 -0400

    Update BirdController.java

--- src/main/java/com/ufinet/birds/controller/BirdController.java
+++ src/main/java/com/ufinet/birds/controller/BirdController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/birds.sql")
+@RequestMapping("/api/birds")
 public class BirdController {
 
     @Autowired
